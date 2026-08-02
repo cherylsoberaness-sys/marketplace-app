2f2491b2f004b085741af921336a6c38b1ccdf88
Cheryl Soberanes <cherylsoberaness@gmail.com>
2026-08-02 15:29:42 -0600

    feat: add search form styles

--- src/app/components/forms/search-ad-form.tsx
+++ src/app/components/forms/search-ad-form.tsx
@@ -7,12 +7,20 @@ export function SearchAdForm({ query, maxPrice, tags }: SearchAdFormProps) {
     const tagsOptions = ['tech', 'home', 'sports', 'gaming', 'furniture'];
 
     return (
-        <form className="flex gap-4" action={"/dashboard"} method="GET">
-            <label htmlFor="query">Search:</label>
-            <input id="query" name="query" className="border" defaultValue={query} />
-
-            <label htmlFor="maxPrice">Max price:</label>
-            <input id="maxPrice" name="maxPrice" className="border" defaultValue={maxPrice} />
+        <form className="mb-8 rounded-2xl border border-dashed border-gray-300 bg-white
+            p-6 shadow-sm space-y-6" action={"/dashboard"} method="GET">
+            
+            <div>
+                <label htmlFor="query" className="mb-2 block text-sm font-medium">Search:</label>
+                <input id="query" name="query" placeholder="¨Search ads..." className="w-full rounded-lg border px-4 py-2
+                    focus:outline-none focus:ring-2" defaultValue={query} />
+            </div>
+            
+            <div>
+                <label htmlFor="maxPrice" className="mb-2 block text-sm font-medium">Max price:</label>
+                <input id="maxPrice" name="maxPrice" className="w-full rounded-lg border px-4 py-2
+                    focus:outline-none focus:ring-2" defaultValue={maxPrice} />
+            </div>
             
             {tagsOptions.map((tag) => (
                 <label key={tag} className="flex items-center gap-2">
