7060888a310e16d6eeb6dd827d057a5f79aa1445
Cheryl Soberanes <cherylsoberaness@gmail.com>
2026-08-04 13:17:23 -0600

    feat: improve delete ad button and modal styles

--- src/app/components/delete-ad-button.tsx
+++ src/app/components/delete-ad-button.tsx
@@ -26,11 +26,11 @@ export function DeleteAdButton({ adId }: DeleteAdButtonProps) {
     
     return (
         <>
-            <button onClick={openModal}>Delete Ad</button>
+            <button onClick={openModal} className="rounded-full border cursor-pointer py-1.5 transition hover hover:bg-gray-200">Delete Ad</button>
             {showModal && (
                 <div className="fixed inset-0 z-50 grid place-items-center bg-black/50 backdrop-blur-sm px-4">
                     <div className="w-full max-w-md flex flex-col items-center gap-6 
-                    rounded-2xl border border-border bg-background p-8 shadow-2xl text-center">
+                    rounded-2xl border border-dashed bg-background p-8 shadow-2xl text-center">
                         <p className="w-full max-w-md font-semibold tracking-tight">Are you sure you want to delete this ad?</p>
                         <div className="mt-2 flex justify-center gap-4">
                             <button className="cursor-pointer rounded-xl border border-border px-4 py-2" type="button" onClick={closeModal}>Cancel</button>
