26881ee076e38d76f59e53fa98bc1fb08c915871
Cheryl Soberanes <cherylsoberaness@gmail.com>
2026-08-05 15:39:22 -0600

    feat: improve navbar responsiveness

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -8,7 +8,7 @@ export default async function Navbar() {
     const session = await getSession();
 
     return (
-        <nav className="relative flex w-full items-center">
+        <nav className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
             <div className="absolute left-1/2 flex -translate-x-1/2 gap-4 text-xs underline underline-offset-4">
                 <Link href="/dashboard">
                     Store
@@ -19,7 +19,7 @@ export default async function Navbar() {
                 {session && <Link href={"/dashboard/ads/create"}>Create ad</Link>}
             </div>
 
-            <div className="ml-auto flex items-center gap-4">
+            <div className="flex items-center gap-3">
                 <UserProfile />
             </div>
         </nav>
